943d890e1f8a143de0dd294505d22ec829d299de
stratsid <sannabhadtisid@gmail.com>
2025-02-22 00:58:19 -0600

    Basic Flask+NextJS Config

---
+++ server/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return jsonify({"message": "Hello, World!"})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+    
